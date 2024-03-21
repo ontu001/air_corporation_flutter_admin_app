d1d3798ea438c96f8d08ef809a70fc3d2a8a2eb2
Shaiful191 <saifulislamridoy1999@gmail.com>
2024-03-21 12:12:10 +0600

    order all api done

--- lib/controller/orders_controller.dart
+++ lib/controller/orders_controller.dart
@@ -133,6 +133,92 @@ class OrderController extends GetxController {
     return false;
   }
 
+  var orderCreteBody = {
+    "customer_name": "Test costomer",
+    "phone_number": "01686381900",
+    "address_details": "Address details",
+    "district": "Dhaka",
+    "order_note": "Any order note",
+    "paid_status": "unpaid", // paid
+    "order_from": "message", // daraz
+    "special_discount": 10,
+    "advance_payment": 50,
+    "delivery_charge": 50,
+    "items": [
+      {
+        "product_name": "Product 1",
+        "product_color": "Red",
+        "product_size": "L",
+        "quantity": 1,
+        "buy_price": 100,
+        "mrp": 200,
+        "sell_price": 150
+      },
+      {
+        "product_name": "Product 1",
+        "product_color": "Green",
+        "product_size": "XL",
+        "quantity": 2,
+        "buy_price": 600,
+        "mrp": 700,
+        "sell_price": 700
+      },
+      {
+        "product_name": "Product 1",
+        "product_color": "Blue",
+        "product_size": "M",
+        "quantity": 3,
+        "buy_price": 1400,
+        "mrp": 1800,
+        "sell_price": 1700
+      }
+    ]
+  };
+
+  Future<bool> orderCreate() async {
+    try {
+      isLoading.value = true;
+      final res = await ApiServices.create(
+        "/admin/orders",
+        orderCreteBody,
+        {
+          "Authorization": "Bearer $token",
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+      );
+
+      // print("aita e >>>>>>> ${res.body}");
+      // print(">>>>>>> ${token}");
+
+      if (res.body != null) {
+        if (res.statusCode == 200) {
+          isLoading.value = false;
+
+          print("aita e orderCretecheck >>>>>>> ${res.body}");
+
+          Map<String, dynamic> jsonData = jsonDecode(res.body);
+
+          //ai khane model crete kore call dite hove;
+           //response na aser karone akhon aibabe diye rakhse;
+          orderUpdatecheck = OrderUpdateModel.fromJson(jsonData);
+
+          // print("status: >>>>>>> ${status.result?.data?[0].bgColor}");
+          // print("status 2: >>>>>>> ${status.result?.data?[0].textColor}");
+          return true;
+        } else {
+          isLoading.value = false;
+          return false;
+        }
+      }
+    } catch (e) {
+      isLoading.value = false;
+      print(e);
+      return false;
+    }
+    return false;
+  }
+
   Future<bool> orderDelete(int id) async {
     try {
       isLoading.value = true;
@@ -155,7 +241,7 @@ class OrderController extends GetxController {
           print("aita e delete er ta >>>>>>> ${res.body}");
 
           Map<String, dynamic> jsonData = jsonDecode(res.body);
-          
+
           //Need update model:
           orderUpdatecheck = OrderUpdateModel.fromJson(jsonData);
 
--- lib/views/add_new_order.dart
+++ lib/views/add_new_order.dart
@@ -1,59 +1,132 @@
+import 'package:air_corporation/controller/orders_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../common/app_color.dart';
 import '../widget/custom_text_fied_for_addorder_editorder.dart';
 
-class AddNewOrder extends StatelessWidget{
+class AddNewOrder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final orderController =
+        Get.find<OrderController>(); // Access the controller
+
+    TextEditingController _1Controller = TextEditingController();
+    TextEditingController _2Controller = TextEditingController();
+    TextEditingController _3Controller = TextEditingController();
+    TextEditingController _4Controller = TextEditingController();
+    TextEditingController _5Controller = TextEditingController();
+    TextEditingController _6Controller = TextEditingController();
+    TextEditingController _7Controller = TextEditingController();
+    TextEditingController _8Controller = TextEditingController();
+    TextEditingController _9Controller = TextEditingController();
+    TextEditingController _10Controller = TextEditingController();
+    TextEditingController _11Controller = TextEditingController();
+    TextEditingController _12Controller = TextEditingController();
+    TextEditingController _13Controller = TextEditingController();
+    TextEditingController _14Controller = TextEditingController();
+    TextEditingController _15Controller = TextEditingController();
+
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(onPressed: ()=>Navigator.pop(context),icon: Icon(Icons.arrow_back,color: kPrimaryColor,),),
-
-          title: Text("Add New Order",style: TextStyle(fontWeight: FontWeight.w900,color: kPrimaryColor),),
-          centerTitle: true,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(
+            Icons.arrow_back,
+            color: kPrimaryColor,
+          ),
         ),
-
-
+        title: Text(
+          "Add New Order",
+          style: TextStyle(fontWeight: FontWeight.w900, color: kPrimaryColor),
+        ),
+        centerTitle: true,
+      ),
       body: Expanded(
         child: ListView(
           scrollDirection: Axis.vertical,
           children: [
             Text("Order Information"),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-
-
-            SizedBox(height: 10,),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _1Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _2Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _3Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _4Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _5Controller,
+            ),
+            SizedBox(
+              height: 10,
+            ),
             Text("Product Information"),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            ElevatedButton(onPressed: (){}, child: Text("ADD")),
-
-
-            SizedBox(height: 10,),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _6Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _7Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _8Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _9Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _10Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _11Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _12Controller,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("ADD")),
+            SizedBox(
+              height: 10,
+            ),
             Text("Regular Price : 0bdt"),
             Text("Discount : 0bdt"),
             Text("Offer Price : 0bdt"),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
-            CustomTextFiledforAddAndEditOrder(),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _13Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _14Controller,
+            ),
+            CustomTextFiledforAddAndEditOrder(
+              controller: _15Controller,
+            ),
             Text("Payable Price: 0bdt"),
-            ElevatedButton(onPressed: (){}, child: Text("ADD ORDER")),
-
-
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+                  onPressed: () async {
+                    //print("object");
+                    bool result = await orderController.orderCreate();
+                    if (result == true) {
+                      Get.snackbar("Order Added", "Order Added Successfully");
+                    } else {
+                      Get.snackbar("Failed", "Something went worng!",
+                          colorText: Colors.red);
+                    }
+                  },
+                  child: Text(
+                    "ADD ORDER",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  )),
+            ),
           ],
         ),
       ),
     );
   }
-
 }
