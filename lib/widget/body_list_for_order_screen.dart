ac47ce2dfed71e9e103375a97c9360ed5abf35fc
ROHANUR RAHMAN <mdrohanurrahmanontu@gmail.com>
2024-03-27 13:02:32 +0600

    In orders page , unnecessery 3 button removed and in status screen edit button disabled

--- lib/widget/body_list_for_order_screen.dart
+++ lib/widget/body_list_for_order_screen.dart
@@ -1,6 +1,7 @@
 import 'package:air_corporation/controller/orders_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import '../common/app_color.dart';
@@ -14,6 +15,7 @@ class BoyListFOrOrderScreen extends StatefulWidget {
 
 class _BoyListFOrOrderScreenState extends State<BoyListFOrOrderScreen> {
   OrderController orderController = OrderController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,153 +36,130 @@ class _BoyListFOrOrderScreenState extends State<BoyListFOrOrderScreen> {
                   color: kPrimaryColor,
                 ),
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    //info
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          // //group 1
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+
+                    Row(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+
+                            IconButton(
+                                onPressed: () async{
+                                  await  Get.to(EditOrder( id:  orderController.orderList.result?.data?[index].id ?? 0));
+                                },
+                                icon: Icon(Icons.edit)),
+
+                            IconButton(
+                                onPressed: () {
+
+
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      title: Text('Warning'),
+                                      content:
+                                      Text('Are you sure you want to delete?'),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () => Navigator.pop(context),
+                                          child: Text('Cancel'),
+                                        ),
+                                        TextButton(
+                                          onPressed: () async {
+                                            // Handle confirm action
+
+                                            var result =
+                                            await controller.orderDelete(controller.orderList.result?.data?[index].id ?? 0);
+                                            if (result == true) {
+                                              Get.snackbar("Deleted",
+                                                  "Item deleted successfully");
+                                              setState(() {
+                                                controller.fetchOrders();
+                                              });
+
+                                            } else {
+                                              Get.snackbar(
+                                                  "Please increse order list",
+                                                  "Please increse order list");
+                                            }
+                                            Navigator.pop(
+                                              context,
+                                            );
+                                          },
+                                          child: Text('Ok'),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                                icon: Icon(
+                                  Icons.delete_outline,
+                                  color: Colors.red,
+                                )),
+                          ],
+                        ),
+                        SizedBox(width: 10.0,),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
                             children: [
-                              Text(
-                                "Order id : ${controller.orderList.result?.data![index].id.toString()}",
-                                style: kTextStyle,
+                              // //group 1
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Order id : ${controller.orderList.result?.data![index].id.toString()}",
+                                    style: kTextStyle,
+                                  ),
+                                  Text(
+                                    "Status : ${controller.orderList.result?.data![index].status?.name.toString()}",
+                                    style: kTextStyle,
+                                  ),
+                                  Text(
+                                    "Phone : ${controller.orderList.result?.data![index].phoneNumber.toString()}",
+                                    style: kTextStyle,
+                                  ),
+                                  Text(
+                                    "Date : ${controller.orderList.result?.data![index].createdAt.toString().substring(0, controller.orderList.result?.data![index].createdAt.toString().indexOf('T'))}",
+                                    style: kTextStyle,
+                                  ),
+                                  Text(
+                                    "Currier : ${controller.orderList.result?.data![index].courierName.toString() == "null" ? " " : controller.orderList.result?.data![index].courierName.toString()}",
+                                    style: kTextStyle,
+                                  ),
+                                  Text(
+                                    "Paid Status : ${controller.orderList.result?.data![index].paidStatus.toString()}",
+                                    style: kTextStyle,
+                                  ),
+                                  Text(
+                                    "Updated by : ${controller.orderList.result?.data![index].updatedBy?.name.toString()}",
+                                    style: kTextStyle,
+                                  ),
+                                  Text(
+                                    "Payable Amount : ${controller.orderList.result?.data![index].payablePrice.toString()}",
+                                    style: kTextStyle,
+                                  ),
+                                ],
                               ),
-                              Text(
-                                "Status : ${controller.orderList.result?.data![index].status?.name.toString()}",
-                                style: kTextStyle,
-                              ),
-                              Text(
-                                "Phone : ${controller.orderList.result?.data![index].phoneNumber.toString()}",
-                                style: kTextStyle,
-                              ),
-                              Text(
-                                "Date : ${controller.orderList.result?.data![index].createdAt.toString().substring(0, controller.orderList.result?.data![index].createdAt.toString().indexOf('T'))}",
-                                style: kTextStyle,
-                              ),
-                              Text(
-                                "Currier : ${controller.orderList.result?.data![index].courierName.toString() == "null" ? " " : controller.orderList.result?.data![index].courierName.toString()}",
-                                style: kTextStyle,
-                              ),
-                              Text(
-                                "Paid Status : ${controller.orderList.result?.data![index].paidStatus.toString()}",
-                                style: kTextStyle,
-                              ),
-                              Text(
-                                "Updated by : ${controller.orderList.result?.data![index].updatedBy?.name.toString()}",
-                                style: kTextStyle,
-                              ),
-                              Text(
-                                "Payable Amount : ${controller.orderList.result?.data![index].payablePrice.toString()}",
-                                style: kTextStyle,
-                              ),
+
+
+
+
                             ],
                           ),
-
-                          SizedBox(
-                            width: 10,
-                          ),
-
-                          //group 2
-                          // Column(
-                          //   crossAxisAlignment: CrossAxisAlignment.start,
-                          //   mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          //   children: [
-                          //     Text(
-                          //       "Currier : ${controller.orderList.result?.data![index].courierName.toString() == "null" ? " " : controller.orderList.result?.data![index].courierName.toString()}",
-                          //       style: kTextStyle,
-                          //     ),
-                          //     Text(
-                          //       "Paid Status : ${controller.orderList.result?.data![index].paidStatus.toString()}",
-                          //       style: kTextStyle,
-                          //     ),
-                          //     Text(
-                          //       "Updated by : ${controller.orderList.result?.data![index].updatedBy?.name.toString()}",
-                          //       style: kTextStyle,
-                          //     ),
-                          //     Text(
-                          //       "Payable Amount : ${controller.orderList.result?.data![index].payablePrice.toString()}",
-                          //       style: kTextStyle,
-                          //     ),
-                          //   ],
-                          // ),
-                        ],
-                      ),
-                    ),
-
-                    //actions
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Checkbox(value: true, onChanged: (value) {}),
-                        IconButton(
-                            onPressed: () async{
-                            await  Get.to(EditOrder( id:  orderController.orderList.result?.data?[index].id ?? 0));
-                            },
-                            icon: Icon(Icons.edit)),
-                        IconButton(onPressed: () {}, icon: Icon(Icons.print)),
-                        IconButton(
-                            onPressed: () {}, icon: Icon(Icons.remove_red_eye)),
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon(Icons.delivery_dining)),
-                        IconButton(
-                            onPressed: () {
-
-
-                              showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  title: Text('Warning'),
-                                  content:
-                                      Text('Are you sure you want to delete?'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () => Navigator.pop(context),
-                                      child: Text('Cancel'),
-                                    ),
-                                    TextButton(
-                                      onPressed: () async {
-                                        // Handle confirm action
-
-                                        var result =
-                                            await controller.orderDelete(controller.orderList.result?.data?[index].id ?? 0);
-                                        if (result == true) {
-                                          Get.snackbar("Deleted",
-                                              "Item deleted successfully");
-                                          setState(() {
-                                            controller.fetchOrders();
-                                          });
-
-                                        } else {
-                                          Get.snackbar(
-                                              "Please increse order list",
-                                              "Please increse order list");
-                                        }
-                                        Navigator.pop(
-                                          context,
-                                        );
-                                      },
-                                      child: Text('Ok'),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                            icon: Icon(
-                              Icons.delete_outline,
-                              color: Colors.red,
-                            )),
+                        ),
                       ],
                     ),
 
-                    //action
-                    Row(
-                      children: [],
-                    )
+
+
+
+
                   ],
                 ),
               );
